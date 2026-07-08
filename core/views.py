4f0aa9ff691f7b6131a7d0b5f4aa0a258a508d71
Zanjo24 <asmeneses1@student.hau.edu.ph>
2026-07-08 23:49:35 +0800

    Fix syntax error and add required list, detail, and personal info views

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import PersonalInformation, Project
 
-def home(request):
+# 1. View that returns all personal information
+def personal_info_view(request):
     personal_info = PersonalInformation.objects.first()
+    return render(request, 'personal_info.html', {'personal_info': personal_info})
+
+# 2. List view for all projects (titles only rendered in HTML)
+def project_list(request):
     projects = Project.objects.all()
-    return render(request, 'index.html', {'personal_info': personal_info, 'projects': projects})
+    return render(request, 'index.html', {'projects': projects})
+
+# 3. Detail view for a single project
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    return render(project_detail.html', {'project': project})
